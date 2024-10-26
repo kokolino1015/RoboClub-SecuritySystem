e3eea3dbd767248d6749d852b3102fceb15ba322
Kaloyan Nedelkov <71516863+kokolino1015@users.noreply.github.com>
2024-10-26 14:06:30 +0300

    Add files via upload

--- SecuritySystem/settings.py
+++ SecuritySystem/settings.py
@@ -80,14 +80,28 @@ WSGI_APPLICATION = 'SecuritySystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'SecuritySystemDB',
+#         'USER': 'postgres',
+#         'PASSWORD': '1123QwER',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project_db',
-        'USER': 'postgres',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SecuritySystemDB',
+        'USER': 'root',
         'PASSWORD': '1123QwER',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
